9664eb1da8ddea45198073c53781753b73d5cc5e
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-05-07 23:40:59 +0900

    feat: 순차적 애니메이션

--- ehddud1006/js/main.js
+++ ehddud1006/js/main.js
@@ -44,9 +44,23 @@ window.addEventListener(
     } else {
       // Badge 요소 보이기!
       gsap.to(badgeEl, 0.6, {
-        opacity: 1,
+        opacity: 0.99,
         display: 'block',
       });
     }
   }, 300),
 );
+
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+// 나타날 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach(function (fadeEl, index) {
+  // 각 요소들을 순서대로(delay) 보여지게 함!
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 0.99,
+  });
+});
