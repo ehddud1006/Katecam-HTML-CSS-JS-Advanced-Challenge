9e639451a8feca352d0d8a88aa777b7a51fd1e24
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-05-08 06:24:06 +0900

    feat: 페이지 상단으로 이동 기능 구현

--- ehddud1006/js/main.js
+++ ehddud1006/js/main.js
@@ -25,6 +25,7 @@ searchInputEl.addEventListener('blur', function () {
  */
 // 페이지 스크롤에 영향을 받는 요소들을 검색!
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 // 페이지에 스크롤 이벤트를 추가!
 // 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
 window.addEventListener(
@@ -39,6 +40,10 @@ window.addEventListener(
         opacity: 0,
         display: 'none',
       });
+      // 상단으로 스크롤 버튼 보이기!
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
 
       // 페이지 스크롤 위치가 500px이 넘지 않으면.
     } else {
@@ -47,9 +52,20 @@ window.addEventListener(
         opacity: 0.99,
         display: 'block',
       });
+      // 상단으로 스크롤 버튼 숨기기!
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
     }
   }, 300),
 );
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 
 /**
  * 순서대로 나타나는 기능
