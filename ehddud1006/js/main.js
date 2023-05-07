9071e1d7a2b58b595908b37d51e9338089093b92
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-05-07 23:02:38 +0900

    feat: 전역 배지

--- ehddud1006/js/main.js
+++ ehddud1006/js/main.js
@@ -19,3 +19,34 @@ searchInputEl.addEventListener('blur', function () {
   searchInputEl.setAttribute('placeholder', '');
   searchInputEl.value = '';
 });
+
+/**
+ * 페이지 스크롤에 따른 요소 제어
+ */
+// 페이지 스크롤에 영향을 받는 요소들을 검색!
+const badgeEl = document.querySelector('header .badges');
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener(
+  'scroll',
+  _.throttle(function () {
+    console.log('test');
+    // 페이지 스크롤 위치가 500px이 넘으면.
+    if (window.scrollY > 500) {
+      // Badge 요소 숨기기!
+      // gsap.to(요소, 지속시간, 옵션)
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+
+      // 페이지 스크롤 위치가 500px이 넘지 않으면.
+    } else {
+      // Badge 요소 보이기!
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300),
+);
