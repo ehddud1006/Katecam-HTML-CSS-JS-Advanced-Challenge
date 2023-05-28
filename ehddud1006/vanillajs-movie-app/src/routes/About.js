acbaf6987fe9887ce5dd0d7e250bb9680a568646
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-05-29 04:47:40 +0900

    feat: 해쉬 라우터 관리 2

--- ehddud1006/vanillajs-movie-app/src/routes/About.js
+++ ehddud1006/vanillajs-movie-app/src/routes/About.js
@@ -2,8 +2,13 @@ import { Component } from '../core/heropy';
 
 export default class Home extends Component {
   render() {
+    const { a, b, c } = history.state;
+
     this.el.innerHTML = /* html */ `
-        <h1>Home Page!</h1>
+        <h1>About Page!</h1>
+        <h2>${a}</h2>
+        <h2>${b}</h2>
+        <h2>${c}</h2>
     `;
   }
 }
--- ehddud1006/vanillajs-movie-app/src/routes/Home.js
+++ ehddud1006/vanillajs-movie-app/src/routes/Home.js
@@ -3,7 +3,7 @@ import { Component } from '../core/heropy';
 export default class About extends Component {
   render() {
     this.el.innerHTML = /* html */ `
-        <h1>About Page!</h1>
+        <h1>Home Page!</h1>
     `;
   }
 }
