4b9ded616871136ee274b7ef3023f41b8b81c0fb
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-06-01 00:11:32 +0900

    feat: 영화 상세 정보 출력

--- ehddud1006/movie-app/src/routes/Movie.js
+++ ehddud1006/movie-app/src/routes/Movie.js
@@ -5,14 +5,14 @@ export default class Movie extends Component {
   async render() {
     this.el.classList.add('container', 'the-movie');
     // 스켈레톤 UI 출력!
-    this.el.innerHTML = /* html */ `
-      <div class="poster skeleton"></div>
-      <div class="specs">
-        <div class="title skeleton"></div>
-        <div class="labels skeleton"></div>
-        <div class="plot skeleton"></div>
-      </div>
-    `;
+    // this.el.innerHTML = /* html */ `
+    //   <div class="poster skeleton"></div>
+    //   <div class="specs">
+    //     <div class="title skeleton"></div>
+    //     <div class="labels skeleton"></div>
+    //     <div class="plot skeleton"></div>
+    //   </div>
+    // `;
     // 영화 상세 정보 가져오기!
     await getMovieDetails(history.state.id);
     const { movie } = movieStore.state;
