4cea86858e204eae7cbcacdd25ab5f4fb6450598
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-05-31 23:33:39 +0900

    feat: 예외 처리와 메시지 출력

--- ehddud1006/movie-app/src/store/movie.js
+++ ehddud1006/movie-app/src/store/movie.js
@@ -6,6 +6,7 @@ const store = new Store({
   pageMax: 1,
   movies: [],
   loading: false,
+  message: 'Search for the movie title!',
 });
 
 export default store;
@@ -13,13 +14,22 @@ export const searchMovies = async page => {
   store.state.loading = true;
   store.state.page = page;
   if (page === 1) {
-    store.state.page = 1;
     store.state.movies = [];
+    store.state.message = '';
   }
-
-  const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
-  const { Search, totalResults } = await res.json();
-  store.state.movies = [...store.state.movies, ...Search];
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
-  store.state.loading = false;
+  try {
+    const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`);
+    const { Response, Search, totalResults, Error } = await res.json();
+    if (Response === 'True') {
+      store.state.movies = [...store.state.movies, ...Search];
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+      store.state.pageMax = 1;
+    }
+  } catch (error) {
+    console.log('searchMovies error:', error);
+  } finally {
+    store.state.loading = false;
+  }
 };
