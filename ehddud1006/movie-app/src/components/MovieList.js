e373f531565013db12e2c66ccbcde4991485f230
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-05-31 19:19:27 +0900

    feat: MovieList 컴포넌트

---
+++ ehddud1006/movie-app/src/components/MovieList.js
@@ -0,0 +1,19 @@
+import { Component } from '../core/heropy';
+import movieStore from '../store/movie';
+
+export default class MovieList extends Component {
+  constructor() {
+    super();
+    movieStore.subscribe('movies', () => this.render());
+  }
+
+  render() {
+    this.el.classList.add('movie-list');
+    this.el.innerHTML = /* html */ `
+        <div class="movies"></div>
+    `;
+
+    const moviesEl = this.el.querySelector('.movies');
+    moviesEl.append(Object.values(movieStore.state.movies).map(movie => movie.Title));
+  }
+}
