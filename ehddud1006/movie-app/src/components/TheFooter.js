a0baccf441659d23e2684097cdc7ca82f5b6c954
KimDongYoung <ehddud1006@pusan.ac.kr>
2023-06-07 01:29:12 +0900

    feat: 내 정보 페이지 출력

--- ehddud1006/movie-app/src/components/TheFooter.js
+++ ehddud1006/movie-app/src/components/TheFooter.js
@@ -1,5 +1,5 @@
 import { Component } from '../core/heropy';
-// import aboutStore from '../store/about';
+import aboutStore from '../store/about';
 
 export default class TheFooter extends Component {
   constructor() {
@@ -8,15 +8,15 @@ export default class TheFooter extends Component {
     });
   }
   render() {
-    // const { github, repository } = aboutStore.state;
+    const { github, repository } = aboutStore.state;
     this.el.innerHTML = /* html */ `
       <div>
-        <a href="">
+        <a href="${repository}">
           GitHub Repository.
         </a>
       </div>
       <div>
-        <a href="">
+        <a href="${github}">
           ${new Date().getFullYear()}
           HEROPY
         </a>
